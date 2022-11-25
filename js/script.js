68355d6f020fc8dc45aa6ff542a87434d0219328
RominaMalimban <romina.lyn@hotmail.it>
2022-11-25 14:26:30 +0100

    esercizio completato

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+const { createApp } = Vue;
+
+createApp({
+    data() {
+      return {
+        title : "Hello world!",
+        myClass : "changeColor"
+      }
+    }
+  }).mount('#app');
